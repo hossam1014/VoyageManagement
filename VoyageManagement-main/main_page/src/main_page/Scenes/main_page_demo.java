aebb01d511fac778188d071ef0577a71f674b6ca
Amir Alsayed <amyralsyd367@gmail.com>
2023-05-20 20:43:52 +0300

    changing main_page_demo

--- VoyageManagement-main/main_page/src/main_page/Scenes/main_page_demo.java
+++ VoyageManagement-main/main_page/src/main_page/Scenes/main_page_demo.java
@@ -1,11 +1,16 @@
 package main_page.Scenes;
 
+import java.util.List;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -15,6 +20,7 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import main_page.Models.Reservation;
 import main_page.Models.User;
 
 /**
@@ -23,13 +29,15 @@ import main_page.Models.User;
  */
 public class main_page_demo extends Scene {
 
+     List<Reservation> reservations;
+    private TableView<Reservation> table;
     private User loggedUser;
 
-//    public main_page_demo(Stage primaryStage) {
-    public main_page_demo(Stage primaryStage, User user) {
+    public main_page_demo(Stage primaryStage) {
+//    public main_page_demo(Stage primaryStage, User user) {
         super(new VBox(), 1250, 760);
 
-        loggedUser = user;
+//        loggedUser = user;
 
         VBox vBox = (VBox) this.getRoot();
         vBox.setAlignment(Pos.CENTER);
@@ -60,18 +68,17 @@ public class main_page_demo extends Scene {
         user_info.setPrefSize(800, 200);
         user_info.setId("user_info");
 
-        GridPane trip_info = new GridPane();
-        trip_info.setPadding(new Insets(20));
-
-        trip_info.setHgap(200);
-        trip_info.setVgap(10);
-        trip_info.setAlignment(Pos.CENTER);
-        trip_info.setPrefSize(800, 200);
-        trip_info.setId("user_info");
+//        GridPane trip_info = new GridPane();
+//        trip_info.setPadding(new Insets(20));
+//
+//        trip_info.setHgap(200);
+//        trip_info.setVgap(10);
+//        trip_info.setAlignment(Pos.CENTER);
+//        trip_info.setPrefSize(800, 200);
+//        trip_info.setId("user_info");
 
         // navigator part of code //
         // adding the photo of the user, dont forget to change adel shakal's photo :)
-
         Circle img_circle = new Circle(75);
         Image user_photo = new Image("images/user_photo.jpg", 300, 300, false, true);
         img_circle.setFill(new ImagePattern(user_photo));
@@ -84,28 +91,28 @@ public class main_page_demo extends Scene {
         Button btn_user_report = new Button("User Reports");
 
         btn_report.setOnAction(e -> {
-//            primaryStage.setScene(new Admin_reports_page(primaryStage));
-            primaryStage.setScene(new Admin_reports_page(primaryStage, loggedUser));
+            primaryStage.setScene(new Admin_reports_page(primaryStage));
+//            primaryStage.setScene(new Admin_reports_page(primaryStage, loggedUser));
         });
 
         btn_travel.setOnAction(e -> {
-//            primaryStage.setScene(new TravellingPage(primaryStage));
-            primaryStage.setScene(new TravellingPage(primaryStage, loggedUser));
+            primaryStage.setScene(new TravellingPage(primaryStage));
+//            primaryStage.setScene(new TravellingPage(primaryStage, loggedUser));
         });
 
         btn_hotel.setOnAction(e -> {
-//            primaryStage.setScene(new Hotel_Resevation(primaryStage));
-            primaryStage.setScene(new Hotel_Resevation(primaryStage, loggedUser));
+            primaryStage.setScene(new Hotel_Resevation(primaryStage));
+//            primaryStage.setScene(new Hotel_Resevation(primaryStage, loggedUser));
         });
 
         btn_tools.setOnAction(e -> {
-//            primaryStage.setScene(new Currency_Converter(primaryStage));
-            primaryStage.setScene(new Currency_Converter(primaryStage, loggedUser));
+            primaryStage.setScene(new Currency_Converter(primaryStage));
+//            primaryStage.setScene(new Currency_Converter(primaryStage, loggedUser));
         });
 
         btn_user_report.setOnAction(e -> {
-//            primaryStage.setScene(new user_reports_page(primaryStage));
-            primaryStage.setScene(new user_reports_page(primaryStage, loggedUser));
+            primaryStage.setScene(new user_reports_page(primaryStage));
+//            primaryStage.setScene(new user_reports_page(primaryStage, loggedUser));
         });
 
 //        btn_hotel.setOnAction(e->{
@@ -118,9 +125,8 @@ public class main_page_demo extends Scene {
         btn_user_report.setMinSize(150, 40);
 
         // there will be some database code here so dont forget hosam  -----------------------------------------------------------------------------------------
-//        Label user_name = new Label("Adel shakal");
-        System.out.println(loggedUser);
-        Label user_name = new Label(this.loggedUser.getFullName());
+        Label user_name = new Label("Adel shakal");
+//        Label user_name = new Label(this.loggedUser.getFullName());
         user_name.setId("user_name_nav");
         user_name.getStyleClass().add("user_name_nav");
 
@@ -150,14 +156,14 @@ public class main_page_demo extends Scene {
 
         // back end and database part -----------------------------------------------------------------------------------------------------------------------------
         // you have to get all these data from sign, login, traveling and hotel pages
-//        TextField txt_full_name = new TextField();
-        TextField txt_full_name = new TextField(loggedUser.getFullName());
-//        TextField txt_user_name = new TextField();
-        TextField txt_user_name = new TextField(loggedUser.getUserName());
-//        TextField txt_email = new TextField();
-        TextField txt_email = new TextField(loggedUser.getEmail());
-//        TextField txt_number = new TextField();
-        TextField txt_number = new TextField(loggedUser.getPhoneNumber());
+        TextField txt_full_name = new TextField();
+//        TextField txt_full_name = new TextField(loggedUser.getFullName());
+        TextField txt_user_name = new TextField();
+//        TextField txt_user_name = new TextField(loggedUser.getUserName());
+        TextField txt_email = new TextField();
+//        TextField txt_email = new TextField(loggedUser.getEmail());
+        TextField txt_number = new TextField();
+//        TextField txt_number = new TextField(loggedUser.getPhoneNumber());
 //        TextField txt_age = new TextField(loggedUser.ge);
 
         txt_user_name.setPrefWidth(400);
@@ -184,19 +190,19 @@ public class main_page_demo extends Scene {
 
         // trip info part
         // some back end and database here also hosam ----------------------------------------------------------------------------------------
-        Label lbl_destination = new Label("Destination");
-        Label lbl_current_location = new Label("Current Location");
-        Label lbl_hotel_name = new Label("Hotel Name");
-        Label lbl_trave_info = new Label("Traveling Information");
-
-        TextField txt_destination = new TextField();
-        TextField txt_current_location = new TextField();
-        TextField txt_hotel_name = new TextField();
+//        Label lbl_destination = new Label("Destination");
+//        Label lbl_current_location = new Label("Current Location");
+//        Label lbl_hotel_name = new Label("Hotel Name");
+////        Label lbl_trave_info = new Label("Traveling Information");
+//
+//        TextField txt_destination = new TextField();
+//        TextField txt_current_location = new TextField();
+//        TextField txt_hotel_name = new TextField();
 
 //        txt_destination.setEditable(false);
 //        txt_current_location.setEditable(false);
 //        txt_hotel_name.setEditable(false);
-        txt_destination.setPrefWidth(400);
+//        txt_destination.setPrefWidth(400);
 
         change.setOnAction(e -> {
             txt_user_name.setEditable(true);
@@ -209,23 +215,52 @@ public class main_page_demo extends Scene {
 
         });
 
-        trip_info.add(lbl_destination, 0, 0);
-        trip_info.add(txt_destination, 1, 0);
-        trip_info.add(lbl_current_location, 0, 2);
-        trip_info.add(txt_current_location, 1, 2);
-        trip_info.add(lbl_hotel_name, 0, 3);
-        trip_info.add(txt_hotel_name, 1, 3);
+//        trip_info.add(lbl_destination, 0, 0);
+//        trip_info.add(txt_destination, 1, 0);
+//        trip_info.add(lbl_current_location, 0, 2);
+//        trip_info.add(txt_current_location, 1, 2);
+//        trip_info.add(lbl_hotel_name, 0, 3);
+//        trip_info.add(txt_hotel_name, 1, 3);
+
+//table part
+table = new TableView<>();
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn<Reservation, String> reservationIdCol = new TableColumn<>("Reservation ID");
+        reservationIdCol.setCellValueFactory(new PropertyValueFactory<>("Id"));
+
+        TableColumn<Reservation, String> reservationDateCol = new TableColumn<>("Reservation Date");
+        reservationDateCol.setCellValueFactory(new PropertyValueFactory<>("ReserveDate"));
+
+        TableColumn<Reservation, String> customerNameCol = new TableColumn<>("Package Name");
+        customerNameCol.setCellValueFactory(new PropertyValueFactory<>("PackageName"));
+
+        TableColumn<Reservation, String> roomNumberCol = new TableColumn<>("Travel Ticket Name");
+        roomNumberCol.setCellValueFactory(new PropertyValueFactory<>("TravelTicketName"));
+
+        TableColumn<Reservation, String> checkInDateCol = new TableColumn<>("Hotel Name");
+        checkInDateCol.setCellValueFactory(new PropertyValueFactory<>("HotelName"));
+
+        TableColumn<Reservation, String> checkOutDateCol = new TableColumn<>("Total");
+        checkOutDateCol.setCellValueFactory(new PropertyValueFactory<>("Total"));
+
+        table.getColumns().addAll(reservationIdCol, reservationDateCol, customerNameCol, roomNumberCol, checkInDateCol, checkOutDateCol);
+//        table.setItems(FXCollections.observableArrayList(reservations));
+        table.getStyleClass().add("rounded-table");
+        
+        
 
         navigator.getChildren().addAll(img_circle, user_name, l1, btn_travel, btn_hotel,
                 btn_tools, btn_user_report);
 //
-        if (user.isIsAdmin()) {
-            navigator.getChildren().add(btn_report); // Add the button to the container
-        } else {
-            navigator.getChildren().remove(btn_report); // Remove the button from the container
-        }
+//        if (user.isIsAdmin()) {
+//            navigator.getChildren().add(btn_report); // Add the button to the container
+//        } else {
+//            navigator.getChildren().remove(btn_report); // Remove the button from the container
+//        }
 
-        info.getChildren().addAll(lbl_user_info, user_info, lbl_trave_info, trip_info);
+//        info.getChildren().addAll(lbl_user_info, user_info, lbl_trave_info, trip_info);
+        info.getChildren().addAll(lbl_user_info, user_info, table);
 
         main_page.getChildren().addAll(navigator, info);
 
